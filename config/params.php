e48aa903a7e31f8a59010ad73a40bf0ba8a1755c
Sasha Kostenko <kostenko289@gmail.com>
2019-10-16 16:31:08 +0300

    Initial commit

---
+++ config/params.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'kostenko289@gmail.com',
+    'senderName' => 'Example.com mailer',
+];
---
+++ modules/admin/controllers/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\modules\admin\controllers;
+use yii\web\Controller;
+
+/**
+ * Default controller for the `admin` module
+ */
+class DefaultController extends AppAdminController
+{
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
